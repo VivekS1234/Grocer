c435679075f26f75a488c74917d6b5aef616e5ad
vivek kumar <v25462432@gmail.com>
2024-07-08 17:26:57 +0530

    Grocery Website UI

---
+++ app.js
@@ -0,0 +1,17 @@
+const icon = document.getElementById('myicon');
+const icon2 = document.getElementById('myicon2');
+const icon3 = document.getElementById('myicon3');
+const icon4 = document.getElementById('myicon4');
+
+icon.addEventListener('click',() =>{
+    icon.classList.toggle('clicked')
+})
+icon2.addEventListener('click',() =>{
+    icon2.classList.toggle('clicked')
+})
+icon3.addEventListener('click',() =>{
+    icon3.classList.toggle('clicked')
+})
+icon4.addEventListener('click',() =>{
+    icon4.classList.toggle('clicked')
+})
